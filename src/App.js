7e5c02345844ea9c31ba24b08c4be1aec5924ecf
Adrienn <adrienntollas@gmail.com>
2020-08-04 16:35:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import red_hair from './img/red.jpeg';
+import logo from './img/logo-final.png'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <img src={logo} alt="" className="logo" />
+
+      <main>
+
+        <img src={red_hair} alt="A woman posing in front of a recycling hill in berlin, germany." />
+        <h1>MARIE LOUISE VOGT</h1>
+      </main>
+
+    </>
   );
 }
 
