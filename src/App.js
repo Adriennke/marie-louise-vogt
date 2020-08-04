aadfcbcc209cbbb16117828635df22a8ce3c26e5
Adrienn <adrienntollas@gmail.com>
2020-08-04 17:24:06 +0200

    create basic layout of landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import red_hair from './img/red.jpeg';
-import logo from './img/logo-final.png'
+import bazaar from './img/Look12.jpeg'
+import logo from './img/logo-final-cropped.png'
 
 import './App.css';
 
@@ -9,17 +10,10 @@ function App() {
   return (
     <>
 
+      <img src={bazaar} alt="A woman posing in front of a recycling hill in berlin, germany." className="main_img" />
+
+
       <img src={logo} alt="" className="logo" />
-
-      <main>
-
-        <img src={red_hair} alt="A woman posing in front of a recycling hill in berlin, germany." />
-        <p>some text to test the font</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur tempora earum sunt, eos corporis debitis at excepturi, non vitae culpa unde quasi nemo sequi optio asperiores esse iste nobis temporibus praesentium iure magni. Deserunt unde, inventore molestiae, eligendi nesciunt cumque tenetur repudiandae sint qui iusto voluptates voluptatum! Eos, praesentium amet aliquid ipsam suscipit, consequuntur, totam enim dolorem itaque vero tenetur nulla atque? Illum consequuntur tempora doloremque perspiciatis quasi, temporibus corporis fugit eos sit, id ab dolorem tenetur magni at laudantium, ullam nesciunt porro! Sit quam debitis hic suscipit consequatur reiciendis modi harum maiores non maxime. Maxime, beatae, modi ducimus aperiam minima, optio esse id itaque suscipit debitis qui quidem nobis numquam quisquam corrupti nihil voluptatibus nisi molestiae officia totam ipsa? Tempora, dolor alias distinctio sunt deserunt facilis corporis porro nam enim explicabo velit accusantium totam temporibus doloribus sapiente doloremque. Ex libero eos nobis error? Quas perferendis, exercitationem facilis totam magnam minima eos qui! Sequi enim expedita reiciendis perferendis pariatur laudantium molestias, dolores voluptates ratione, quis reprehenderit ea magni consequuntur id minima necessitatibus ex ut officiis quidem atque recusandae eligendi? Vitae cum totam nesciunt perferendis. Excepturi, veniam omnis nesciunt ex sequi totam assumenda nemo quaerat iste, sit nostrum cumque labore praesentium cum libero odit. Incidunt voluptas mollitia pariatur provident ad accusantium voluptatem. Officia cupiditate eius possimus fugit provident sapiente illo vitae aspernatur ut voluptatum assumenda doloremque distinctio laudantium consequuntur, a labore consectetur dolores repellat recusandae? Facere in explicabo odio. Tempora necessitatibus recusandae iusto inventore nostrum odit eos accusantium cum a facere, beatae, consequatur earum excepturi ad officiis alias corporis! Molestiae expedita vero placeat nihil numquam deserunt, vel minima reiciendis ipsam. Nostrum voluptatum voluptas quibusdam ipsa nam sed sit nemo esse ullam. Amet deleniti illo quaerat tempore at! Cupiditate, alias! Laudantium nostrum harum odit velit cum quas consequuntur, rem minus ab.
-        </p>
-
-      </main>
 
     </>
   );
