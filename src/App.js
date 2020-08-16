0801a7a974f87315163f822e36a60ddf51b9f946
Adrienn <adrienntollas@gmail.com>
2020-08-16 18:02:28 +0200

    add gif, add smooth scrolling and document title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 import './App.scss';
@@ -13,9 +12,9 @@ function App() {
     <>
       <div className="container">
         <Home />
-        <About />
-        <Contact />
       </div>
+      <About />
+      <Contact />
     </>
   );
 }
